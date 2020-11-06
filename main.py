0e822a5e097f37bbec0aae18fb5f4c49fd68b86b
Maja Minnaert <maja.minnaert@gmail.com>
2020-11-06 12:16:34 +0100

    say hi

--- main.py
+++ main.py
@@ -7,8 +7,7 @@
 # Project Repository:
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Coach')
