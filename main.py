528479edc5fb42b6145d6444276f697e3a111630
Maja Minnaert <maja.minnaert@gmail.com>
2020-11-06 12:00:59 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Main file of SeeMyHouse project
+# Made in assignment with learning
+# and evaluation purposes for BeCode
+# training "AI Bootcamp" Gent
+# Written by Maja Minnaert
+# Project page on Github:
+# Project Repository:
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
